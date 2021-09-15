99782c40e54e2f64bd595d76e9134f70b63252b1
Gabriel Wojcik <62144072+GabrielWojcik@users.noreply.github.com>
2021-09-15 20:55:10 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastroveiculo/CadastroController.java
@@ -0,0 +1,11 @@
+package com.example.cadastroveiculo;
+
+import javafx.event.ActionEvent;
+
+public class CadastroController {
+    //Definição do onAction
+    public void cadastro(ActionEvent e){
+        System.out.println("Botão selecionado");
+
+    }
+}
